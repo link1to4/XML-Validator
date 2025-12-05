9eedb6fc8d4cafd07a7430a32b63bdb20bf91dc9
Your Name <you@example.com>
2025-12-05 15:38:35 +0800

    Update config for Render deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // TODO: 如果你的 repo 名稱是 xml-validator，請填寫 '/xml-validator/'
   // 如果你是部署到 username.github.io (User Page)，則填寫 '/'
-  base: '/XML-Validator/', 
+  base: '/', 
   server: {
     port: 80,
   },
